d2ac5bd6c9ef3af515748c774e54ef74c19e114f
Yunjae <nuguri206@gmail.com>
2023-01-08 23:41:17 +0900

    no message

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Chat from "../pages/chat/Chat";
-import ChatForm from "../pages/chat/components/ChatForm";
-import PetInfo from "../pages/firstLogin/components/PetInfo";
-import FirstLogin from "../pages/firstLogin/FirstLogin";
 import EmaliLogin from "../pages/login/components/EmailLogin";
 import KakaoLogin from "../pages/login/components/KakaoLogin";
 import NaverLogin from "../pages/login/components/NaverLogin";
 import Login from "../pages/login/Login";
-import Signup from "../pages/signup/Signup";
 // import Home from "../pages/home/Home";
 const Router = () => {
   return(
@@ -16,15 +11,9 @@ const Router = () => {
       <Routes>
         {/* <Route path="/" exact element={<Home />} /> */}
         <Route path="/login" exact element={<Login />} />
-        <Route path="/signup" exact element={<Signup />} />
         <Route path="/emaillogin" exact element={<EmaliLogin />} />
-        <Route path="/firstlogin" exact element={<FirstLogin />} />
-        <Route path="/firstlogin/pet" exact element={<PetInfo />} />
         <Route path="/user/kakao/callback" exact element={<KakaoLogin />} />
         <Route path="/login/oauth2/code/naver" exact element={<NaverLogin />} />
-
-        <Route path="/chat" exact element={<Chat />} />
-        <Route path="/chatform" exact element={<ChatForm />} />
       </Routes>
     </BrowserRouter>
   );
